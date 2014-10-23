a9c883bdb3896724549cb8bf6d9ed9b2d00f496f
Matt Sayler <matt.sayler+code@gmail.com>
2014-10-23 15:00:07 -0700

    Added code comments

--- maptimelogoify.js
+++ maptimelogoify.js
@@ -1,8 +1,9 @@
 // function to maptime logo-ify some text. Requires jQuery
 
 function MaptimeLogoify(logoText, targetClass, targetClassStyle) {
-  logoText = typeof logoText !== 'undefined' ? logoText : "#maptime!"
-  targetClass = typeof targetClass != 'undefined' ? targetClass : "logo"
+  logoText = typeof logoText !== 'undefined' ? logoText : "#maptime!"; //Default text
+  targetClass = typeof targetClass != 'undefined' ? targetClass : "logo"; //Default target div class
+  
   var target = "div." + targetClass;
   var colors = [["red","#ff0000"], 
                 ["orange","#ffa500"], 
@@ -11,6 +12,10 @@ function MaptimeLogoify(logoText, targetClass, targetClassStyle) {
                 ["blue","#00dddd"], 
                 ["indigo","#800080"]];
   
+  // Append & style a div for each color to the target.
+  // The color names (i.e. "indigo") are used for div
+  // classes in case you want to do some additional
+  // styling or add animation, etc.
   for (i = 0; i < 6; i++ ) {
     var cssAttribs = {"color":colors[i][1], 
                      "z-index":"inherit+" + String(i), 
@@ -25,6 +30,10 @@ function MaptimeLogoify(logoText, targetClass, targetClassStyle) {
                       .css(cssAttribs)
                     );
   };
+  
+  // Optionally, styling for the target div can be passed in via 
+  // the 'targetClassStyle' parameter (instead of defining it in 
+  // the head or a css doc)
   if (targetClassStyle !== undefined & targetClassStyle !== null & targetClassStyle !== "") {
     $(target).css(targetClassStyle);
   };
